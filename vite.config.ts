a3127b725eca476cf6fb97209cbaf1f921fa455a
huangbo <huangbo@lj-ch.com>
2025-01-08 10:57:24 +0800

    feat: 项目初始化

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { VantResolver } from '@vant/auto-import-resolver'
+import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { defineConfig } from 'vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+
+  plugins: [vue(), AutoImport({
+    resolvers: [VantResolver()],
+  }), Components({
+    resolvers: [VantResolver()],
+  })],
+})
